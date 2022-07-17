bcba5d2950ae163f67968abc20a1017910672474
Yuta <y.sugiyama54@gmail.com>
2022-07-17 10:55:58 +0900

    create employeeservice with savedEmployee method

---
+++ src/main/java/net/javaguides/springboottesting/service/EmployeeService.java
@@ -0,0 +1,7 @@
+package net.javaguides.springboottesting.service;
+
+import net.javaguides.springboottesting.model.Employee;
+
+public interface EmployeeService {
+    Employee saveEmployee(Employee employee);
+}
---
+++ src/main/java/net/javaguides/springboottesting/service/impl/EmployeeServiceImpl.java
@@ -0,0 +1,28 @@
+package net.javaguides.springboottesting.service.impl;
+
+import net.javaguides.springboottesting.exception.ResourceNotFoundException;
+import net.javaguides.springboottesting.model.Employee;
+import net.javaguides.springboottesting.repository.EmployeeRepository;
+import net.javaguides.springboottesting.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @Override
+    public Employee saveEmployee(Employee employee) {
+
+        Optional<Employee> savedEmployee = employeeRepository.findByEmail(employee.getEmail());
+        if(savedEmployee.isPresent()) {
+            throw new ResourceNotFoundException("Employee already exist with given email:" + employee.getEmail());
+        }
+
+        return employeeRepository.save(employee);
+    }
+}
