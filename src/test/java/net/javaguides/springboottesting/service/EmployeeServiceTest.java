8f243bc547dd7b716eaf8753a5a27406d17bf040
Yuta <y.sugiyama54@gmail.com>
2022-07-19 22:25:35 +0900

    unit test for saveEmployee method which throws exception

--- src/test/java/net/javaguides/springboottesting/service/EmployeeServiceTest.java
+++ src/test/java/net/javaguides/springboottesting/service/EmployeeServiceTest.java
@@ -1,5 +1,6 @@
 package net.javaguides.springboottesting.service;
 
+import net.javaguides.springboottesting.exception.ResourceNotFoundException;
 import net.javaguides.springboottesting.model.Employee;
 import net.javaguides.springboottesting.repository.EmployeeRepository;
 import net.javaguides.springboottesting.service.impl.EmployeeServiceImpl;
@@ -7,7 +8,12 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
 
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -42,7 +48,7 @@ public class EmployeeServiceTest {
     public void givenEmployeeObject_whenSaveEmployee_thenReturnEmployeeObject() {
         // given - precondition or setup
         given(employeeRepository.findByEmail(employee.getEmail()))
-                .willReturn(Optional.empty());
+                .willReturn(Optional.of(employee));
 
         given(employeeRepository.save(employee))
                 .willReturn(employee);
@@ -52,5 +58,21 @@ public class EmployeeServiceTest {
 
         // then - verify the output
         Assertions.assertThat(savedEmployee).isNotNull();
+    }
+
+    @Test
+    @DisplayName("jUnit test for saveEmployee method which throws exception")
+    public void givenExistingEmail_whenSaveEmployee_thenThrowsException() {
+        // given - precondition or setup
+        given(employeeRepository.findByEmail(employee.getEmail()))
+                .willReturn(Optional.of(employee));
+
+        // when - action or the behaviour that we are going test
+        assertThrows(ResourceNotFoundException.class, () -> {
+            employeeService.saveEmployee(employee);
+        });
+
+        // then - verify the output
+        verify(employeeRepository, never()).save(any(Employee.class));
     }
 }
