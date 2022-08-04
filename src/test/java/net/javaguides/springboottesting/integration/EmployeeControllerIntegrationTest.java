f634523a6cf494ef205842b49588040fec71798e
Yuta <y.sugiyama54@gmail.com>
2022-08-05 00:45:05 +0900

    Integration test for create employee REST API

---
+++ src/test/java/net/javaguides/springboottesting/integration/EmployeeControllerIntegrationTest.java
@@ -0,0 +1,65 @@
+package net.javaguides.springboottesting.integration;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import net.javaguides.springboottesting.model.Employee;
+import net.javaguides.springboottesting.repository.EmployeeRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+public class EmployeeControllerIntegrationTest {
+
+    // Injecting MockMvc class to make HTTP request using perform() method
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @BeforeEach
+    void setup() {
+        employeeRepository.deleteAll();
+    }
+
+    @Test
+    @DisplayName("jUnit test for saveEmployee REST API")
+    public void givenEmployeeObject_whenCreateEmployee_thenReturnSavedEmployee() throws Exception {
+        // given - precondition or setup
+        Employee employee = Employee.builder()
+                .firstName("yuta")
+                .lastName("sugiyama")
+                .email("hoge@gmail.com")
+                .build();
+
+        // when - action or the behaviour that we are going test
+        ResultActions response = mockMvc.perform(post("/api/employee")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(employee)));
+
+        // then - verify the output
+        response.andDo(MockMvcResultHandlers.print())
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.firstName", is(employee.getFirstName())))
+                .andExpect(jsonPath("$.lastName", is(employee.getLastName())))
+                .andExpect(jsonPath("$.email", is(employee.getEmail())));
+    }
+}
